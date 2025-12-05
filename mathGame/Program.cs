b7201a8dcd73aa0e5f939d822de12da1d2c7adde
KitCat <103929780+KitCatJ@users.noreply.github.com>
2025-12-05 12:47:07 +0000

    added conversion of user input to int and comparasion to see if the answer is right or wrong; also added a point system

--- mathGame/Program.cs
+++ mathGame/Program.cs
@@ -2,6 +2,8 @@
 
 int questions = 5;
 string question = "null";
+int answer = 0;
+int points = 0;
 
 
 for (int i = 0; i < questions; i++) {
@@ -11,15 +13,18 @@ for (int i = 0; i < questions; i++) {
     
 
     if (signal == 0) {
-        question = $"How much is {num1} + {num2}"; 
+        question = $"How much is {num1} + {num2}";
+        answer = num1 + num2;
     }
     
     else if (signal == 1) {
        question = $"How much is {num1} - {num2}";
+       answer = num1 - num2;
     }
     
     else if (signal == 2) {
         question = $"How much is {num1} * {num2}";
+        answer = num1 * num2;
     }
     else {
         while (num1 % num2 != 0)
@@ -29,8 +34,31 @@ for (int i = 0; i < questions; i++) {
         }
 
         question = $"How much is {num1} / {num2}"; 
+        answer = num1 / num2;
         
     }
+    
     Console.WriteLine(question);
-    string answer = Console.ReadLine();
+    string userAnswer = Console.ReadLine();
+    
+    if (int.TryParse(userAnswer, out int userAnswerInt))
+    {
+        if (userAnswerInt == answer)
+        {
+            Console.WriteLine("Correct!");
+            points++;
+            Console.WriteLine($"Points: {points}");
+        }
+        else
+        {
+            Console.WriteLine($"Incorrect!\nThe answer is {answer}");
+            Console.WriteLine($"Points: {points}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("That is not a valid number!");
+    }
 }
+
+Console.WriteLine($"Game over\nYou finished with {points} points!");
