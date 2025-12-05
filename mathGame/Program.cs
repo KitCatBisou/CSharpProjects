0e5827a4a369895b66c4345db31a7114b553fc2a
KitCat <103929780+KitCatJ@users.noreply.github.com>
2025-12-05 12:12:07 +0000

    added if statements for which operator the question is gonna have

--- mathGame/Program.cs
+++ mathGame/Program.cs
@@ -1,5 +1,38 @@
 ﻿Random rnd = new Random();
 
-int dividend = rnd.Next(1, 100);
-Console.Write(dividend);
+int questions = 5;
+string question = "null";
 
+
+for (int i = 0; i < questions; i++) {
+    int num1 = rnd.Next(1, 100);
+    int num2 = rnd.Next(1, 100);
+    int signal = rnd.Next(3);
+    
+
+    if (signal == 0) {
+        question = $"How much is {num1} + {num2}"; 
+    }
+    
+    else if (signal == 1) {
+       question = $"How much is {num1} - {num2}";
+    }
+    
+    else if (signal == 2) {
+        question = $"How much is {num1} * {num2}";
+    }
+    else {
+        question = $"How much is {num1} / {num2}";
+    }
+    Console.WriteLine(question);
+    string answer = Console.ReadLine();
+    
+    /*
+    if (dividend1 % dividend2 == 0)
+    {
+        string question = $"How much is {dividend1} x {dividend2}";
+        Console.WriteLine(question);
+        string answer = Console.ReadLine();
+    }
+*/
+}
--- mathGame/obj/Debug/net10.0/mathGame.AssemblyInfo.cs
+++ mathGame/obj/Debug/net10.0/mathGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mathGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9094328ceaa6b270a3ed0a33cea7478cd02f73")]
 [assembly: System.Reflection.AssemblyProductAttribute("mathGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mathGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
