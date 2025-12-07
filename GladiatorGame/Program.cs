852958cd7f223680e7205e101e73f0b700d82469
KitCat <103929780+KitCatJ@users.noreply.github.com>
2025-12-07 10:56:24 +0000

    initial code for gladiator game

---
+++ GladiatorGame/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GladiatorArena
+{
+	class Gladiator
+	{
+		public string Name;
+		public int Health;
+		public int Damage;
+
+		public Gladiator(string incomingName, int incomingHealth, int incomingDamage)
+		{
+			Name = incomingName;
+			Health = incomingHealth;
+			Damage = incomingDamage;
+		}
+		public void Attack(Gladiator enemy)
+		{
+			//make a function for attacking an npc
+		}
+
+	}
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+};
