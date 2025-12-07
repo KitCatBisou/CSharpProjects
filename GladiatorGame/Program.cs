9a52a623f058230b872d093d30c91d84039402c5
KitCat <103929780+KitCatJ@users.noreply.github.com>
2025-12-07 11:34:41 +0000

    added rng system to hits

--- GladiatorGame/Program.cs
+++ GladiatorGame/Program.cs
@@ -4,6 +4,7 @@ namespace GladiatorArena
 {
 	class Gladiator
 	{
+		static Random rnd = new Random();
 		public string Name;
 		public int Health;
 		public int Damage;
@@ -16,8 +17,25 @@ namespace GladiatorArena
 		}
 		public void Attack(Gladiator enemy)
 		{
-			enemy.Health -= Damage;
-			Console.WriteLine($"{Name} hits {enemy.Name} for {Damage} damage!");
+			int hitvalue = rnd.Next(Damage - 2, Damage + 2);
+			int chance = rnd.Next(1, 101);
+			
+			if (chance > 0 && chance < 15)
+			{
+				hitvalue = 0;
+				Console.WriteLine($"{Name} misses!");
+			}
+			else if (chance > 95 && chance <= 100)
+			{
+				hitvalue *= 2;
+				enemy.Health -= hitvalue;
+				Console.WriteLine($"Critical hit!\n{Name} hits {enemy.Name} for {hitvalue} damage!");
+			}
+			else
+			{
+				enemy.Health -= hitvalue;
+				Console.WriteLine($"{Name} hits {enemy.Name} for {hitvalue} damage!");
+			}
 		}
 
 	}
@@ -26,7 +44,23 @@ namespace GladiatorArena
 	{
 		static void Main(string[] args)
 		{
-			Gladiator main = new Gladiator("Cratus", 100, 10);
+			Gladiator hero = new Gladiator("Cratus", 100, 10);
+			Gladiator enemy = new Gladiator("Maximus", 100, 8);
+
+			while (hero.Health > 0 && enemy.Health > 0)
+			{
+				hero.Attack(enemy);
+
+				if (enemy.Health <= 0)
+				{
+					Console.WriteLine("Victory!");
+					break;
+				}
+				enemy.Attack(hero);
+				
+				
+			}
+			Console.WriteLine("Game over!");
 		}
 	}
 };
