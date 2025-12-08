dcddee26ed1fa16807f1988300f0a41e864cef5a
KitCat <103929780+KitCatJ@users.noreply.github.com>
2025-12-08 16:39:10 +0000

    added a few weapons and added defense to gladiator class

--- GladiatorGame/Program.cs
+++ GladiatorGame/Program.cs
@@ -3,10 +3,12 @@
  */
 
 /*
- Add armor system and different armors and weapons
- At the end of each fight player gets a choice between an armor or a weapon
- maybe big lists with rng percentages for each item?
- fighters get stronger each level
+ TODO:
+ -Add more weapons and armor
+ -implement the attack and defense bonuses on the gameplay
+ -At the end of each fight player gets a choice between an armor or a weapon
+ -maybe big lists with rng percentages for each item?
+ -fighters get stronger each level
  */
 
 using System;
@@ -53,15 +55,16 @@ namespace GladiatorArena
 		public string Name;
 		public int Health;
 		public int Damage;
-		public int Armor;
+		public int Defense;
 		
 		
 		//Gladiator Constructor
-		public Gladiator(string incomingName, int incomingHealth, int incomingDamage)
+		public Gladiator(string incomingName, int incomingHealth, int incomingDamage, int incomingDefense)
 		{
 			Name = incomingName;
 			Health = incomingHealth;
 			Damage = incomingDamage;
+			Defense = incomingDefense;
 		}
 		
 		public void Attack(Gladiator enemy)
@@ -71,19 +74,18 @@ namespace GladiatorArena
 			
 			if (chance > 0 && chance < 15)
 			{
-				hitvalue = 0;
 				Console.WriteLine($"{Name} misses!");
 			}
 			else if (chance > 95 && chance <= 100)
 			{
 				hitvalue *= 2;
-				enemy.Health -= hitvalue;
+				enemy.Health = enemy.Health - (hitvalue - Defense);
 				Console.ForegroundColor = ConsoleColor.Red;
 				Console.WriteLine($"Critical hit!\n{Name} hits {enemy.Name} for {hitvalue} damage!");
 			}
 			else
 			{
-				enemy.Health -= hitvalue;
+				enemy.Health = enemy.Health - (hitvalue - Defense);
 				Console.ForegroundColor = ConsoleColor.Cyan;
 				Console.WriteLine($"{Name} hits {enemy.Name} for {hitvalue} damage!");
 			}
@@ -107,7 +109,10 @@ namespace GladiatorArena
 		{
 			List<Weapon> weapons = new List<Weapon>();
 			
+			weapons.Add(new Weapon("Club", 1));
 			weapons.Add(new Weapon("Dagger", 2));
+			weapons.Add(new Weapon("Sickle", 3));
+			
 			return weapons;
 		}
 
@@ -147,8 +152,8 @@ namespace GladiatorArena
 			Console.Title = "Gladiator Arena";
 			Console.WriteLine("Name your fighter: ");
 			string userName = Console.ReadLine();
-			Gladiator hero = new Gladiator(userName, 100, 10);
-			Gladiator enemy = new Gladiator("Maximus", 100,10);
+			Gladiator hero = new Gladiator(userName, 100, 10, 0);
+			Gladiator enemy = new Gladiator("Maximus", 100,10, 0);
 			
 			//GAME BEGINS
 			
